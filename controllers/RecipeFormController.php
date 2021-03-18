8be449e9177c209d276b10eb385fa8a2a5fbd911
alex hawley <alex@engage2learn.org>
2021-03-18 13:09:35 -0500

    - Finished work on editing recipes and updating new recipe fields. Still need to be able to update Ingredient Price - Cheap / Expensive

--- controllers/RecipeFormController.php
+++ controllers/RecipeFormController.php
@@ -10,6 +10,8 @@ use app\models\RiIngredientType;
 use app\models\RiProtein;
 use app\models\RiRecipe;
 use app\models\RiRecipeIngredient;
+use app\models\RiRecipeStyle;
+use app\models\RiTasteLevel;
 use app\services\Utils;
 use Bills;
 use DateTime;
@@ -67,8 +69,10 @@ class RecipeFormController extends Controller
         $last_date_made2 = Yii::$app->request->post("last_date_made", 0);
         $contains_salad = Yii::$app->request->post("contains_salad", 0);
         $contains_gluten = Yii::$app->request->post("contains_gluten", 0);
-        $protein_id = Yii::$app->request->post("protein_id", 0);
-        $difficulty_level_id = Yii::$app->request->post("difficulty_level_id", 0);
+        $protein_id = Yii::$app->request->post("protein_id");
+        $difficulty_level_id = Yii::$app->request->post("difficulty_level_id");
+        $taste_level_id = Yii::$app->request->post("taste_level_id");
+        $recipe_style_id = Yii::$app->request->post("recipe_style_id");
         $is_homechef = Yii::$app->request->post("is_homechef", 0);
         $is_easy = Yii::$app->request->post("is_easy", 0);
 
@@ -85,6 +89,65 @@ class RecipeFormController extends Controller
         $RiRecipe->contains_gluten = $contains_gluten;
         $RiRecipe->protein_id = $protein_id;
         $RiRecipe->difficulty_level_id = $difficulty_level_id;
+        $RiRecipe->taste_level_id = $taste_level_id;
+        $RiRecipe->recipe_style_id = $recipe_style_id;
+        $RiRecipe->is_homechef = $is_homechef;
+        $RiRecipe->is_easy = $is_easy;
+
+        $valid = $RiRecipe->validate();
+        if (!$valid) {
+            throw new \yii\web\NotFoundHttpException('Invalid entry: ' . print_r($RiRecipe->getErrors(), true));
+        }
+        $RiRecipe->save();
+
+        header("Access-Control-Allow-Origin: {$this->allowedOriginDomain}");
+        header("Content-type: text/json");
+
+        echo json_encode([
+            "item" => $RiRecipe->toArray()
+        ]);
+        die();
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionUpdate()
+    {
+        $id = Yii::$app->request->get("id", 0);
+
+        $title = Yii::$app->request->post("title", "");
+        $rating = Yii::$app->request->post("rating", 1);
+        $last_date_made2 = Yii::$app->request->post("last_date_made", 0);
+        $contains_salad = Yii::$app->request->post("contains_salad", 0);
+        $contains_gluten = Yii::$app->request->post("contains_gluten", 0);
+        $protein_id = Yii::$app->request->post("protein_id");
+        $difficulty_level_id = Yii::$app->request->post("difficulty_level_id");
+        $taste_level_id = Yii::$app->request->post("taste_level_id");
+        $recipe_style_id = Yii::$app->request->post("recipe_style_id");
+        $is_homechef = Yii::$app->request->post("is_homechef", 0);
+        $is_easy = Yii::$app->request->post("is_easy", 0);
+
+        $last_date_made = "";
+        if ($last_date_made2 > 0) {
+            $last_date_made = date("Y-m-d H:i:s", $last_date_made2);
+        }
+
+        $RiRecipe = RiRecipe::findOne($id);
+        if (!$RiRecipe) {
+            throw new \yii\web\NotFoundHttpException('Recipe not found');
+        }
+        $RiRecipe->title = $title;
+        $RiRecipe->rating = $rating;
+        $RiRecipe->last_date_made = $last_date_made;
+        $RiRecipe->contains_salad = $contains_salad;
+        $RiRecipe->contains_gluten = $contains_gluten;
+        $RiRecipe->protein_id = $protein_id;
+        $RiRecipe->difficulty_level_id = $difficulty_level_id;
+        $RiRecipe->taste_level_id = $taste_level_id;
+        $RiRecipe->recipe_style_id = $recipe_style_id;
         $RiRecipe->is_homechef = $is_homechef;
         $RiRecipe->is_easy = $is_easy;
 
@@ -112,6 +175,52 @@ class RecipeFormController extends Controller
 
         echo json_encode([
             "items" => $RiProteins
+        ]);
+        die();
+    }
+
+    public function actionRecipeStyles()
+    {
+        $RiRecipeStyles = RiRecipeStyle::find()->asArray()->all();
+
+        header("Access-Control-Allow-Origin: {$this->allowedOriginDomain}");
+        header("Content-type: application/json");
+
+        echo json_encode([
+            "items" => $RiRecipeStyles
+        ]);
+        die();
+    }
+
+    public function actionTasteLevels()
+    {
+        $RiTasteLevels = RiTasteLevel::find()->asArray()->all();
+
+        header("Access-Control-Allow-Origin: {$this->allowedOriginDomain}");
+        header("Content-type: application/json");
+
+        echo json_encode([
+            "items" => $RiTasteLevels
+        ]);
+        die();
+    }
+
+    public function actionView($id)
+    {
+
+        $recipe = RiRecipe::find()->where(['id' => $id])->asArray()->one();
+        if ($recipe) {
+            $recipe['contains_gluten'] = intval($recipe['contains_gluten']);
+            $recipe['contains_salad'] = intval($recipe['contains_salad']);
+            $recipe['is_homechef'] = intval($recipe['is_homechef']);
+            $recipe['is_easy'] = intval($recipe['is_easy']);
+        }
+
+        header("Access-Control-Allow-Origin: {$this->allowedOriginDomain}");
+        header("Content-type: application/json");
+
+        echo json_encode([
+            "item" => $recipe
         ]);
         die();
     }
